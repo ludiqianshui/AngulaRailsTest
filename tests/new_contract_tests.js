6586993db2d3642263874cc5d9f4d47b6ecf304e
Qiguang Song <qiguang.song@nl.abnamro.com>
2018-11-05 13:05:33 +0100

    add test for log-in

---
+++ tests/new_contract_tests.js
@@ -0,0 +1,13 @@
+var assert = require ('assert');
+describe('Login Page valid', test1 () {
+  Newcontract ("firstName", "lastName", "2011-11-11", "firstName@gmail.com")
+  it('should let you log in', function () {
+    contractName = browser.getText("#employee-list > li:nth-child(1)");
+    assert.equal(contract, "firstName" + " " + "lastName")
+}}
+describe('Login Page invalid date', test2 () {
+  Newcontract ("firstName", "lastName", "2011", "firstName@gmail.com")
+  it('should let you log in', function () {
+    contractName = browser.getText("#employee-list > li:nth-child(1)");
+    assert.equal(contract, "firstName" + " " + "lastName")
+}}
